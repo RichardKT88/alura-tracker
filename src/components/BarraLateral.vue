bc94cb69924ccd98eaa8cb7e35128672acc04825
RICHARD TANAKA <richard.tanaka@viavarejo.com.br>
2022-09-12 16:15:04 -0300

    [Done] 01. Bem começado, metade feito

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -1,11 +1,29 @@
 <template>
   <header>
-    <h1>
-      <img src="../assets/logo.png" alt="" />
-    </h1>
-    <button class="button" @click="alterarTema">
-      {{textoBotao}}
-    </button>
+    <div class="has-text-centered	">
+      <h1>
+        <img src="../assets/logo.png" alt="" />
+      </h1>
+      <button class="button" @click="alterarTema">
+        {{textoBotao}}
+      </button>
+    </div>
+    <nav class="panel mt-5">
+      <ul>
+        <li>
+          <router-link to="/" class="link">
+            <i class="fas fa-tasks"></i>
+            tarefas
+          </router-link>
+        </li>
+        <li>
+          <router-link to="/projetos" class="link">
+            <i class="fas fa-project-diagram"></i>
+            projetos
+          </router-link>
+        </li>
+      </ul>
+    </nav>
   </header>
 </template>
 
@@ -15,20 +33,20 @@ import { defineComponent } from "vue";
 export default defineComponent({
   name: "BarraLateral",
   emits: ['aoTemaAlterado'],
-  data () {
+  data() {
     return {
       modoEscuroAtivo: false
     }
   },
   methods: {
-    alterarTema(){
+    alterarTema() {
       this.modoEscuroAtivo = !this.modoEscuroAtivo
       this.$emit('aoTemaAlterado', this.modoEscuroAtivo)
     }
   },
   computed: {
-    textoBotao () {
-      if (this.modoEscuroAtivo){
+    textoBotao() {
+      if (this.modoEscuroAtivo) {
         return 'Desativar modo escuro'
       }
       return 'Ativar modo escuro'
@@ -42,13 +60,28 @@ header {
   background: #0d3b66;
   width: 100%;
   height: 100vh;
-  text-align: center;
+}
+
+.panel li {
+  margin: 8px 0;
+}
+
+.link {
+  color: #fff;
+}
+
+.link:hover {
+  color: #FAF0CA;
+}
+
+.link.router-link-active {
+  color: #FAF0CA;
 }
 
 @media only screen and (max-width: 768px) {
   header {
     padding: 2.5rem;
-    height: auto;    
+    height: auto;
   }
 }
 </style>
