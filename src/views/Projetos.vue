9fb27f458677f1f4c3b5acbba748882baeff5391
RICHARD TANAKA <richard.tanaka@viavarejo.com.br>
2022-09-14 17:18:00 -0300

    [Done] 02. Gerenciando o estado

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -40,25 +40,27 @@
 </template>
 
 <script lang="ts">
-import type IProjeto from '@/interfaces/IProjeto';
-import { defineComponent } from 'vue';
+import { useStore } from '@/store';
+import { computed, defineComponent } from 'vue';
 
 export default defineComponent({
     name: 'ProjetosVue',
     data () {
         return {
             nomeDoProjeto: '',
-            projetos: [] as IProjeto[]
         };
     },
     methods: {
         salvar () {
-            const projeto: IProjeto = {
-                nome: this.nomeDoProjeto,
-                id: new Date().toISOString()
-            }
-            this.projetos.push(projeto)
-            this.nomeDoProjeto = ''
+           this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
+           this.nomeDoProjeto = "";
+        }
+    },
+    setup() {
+        const store = useStore()
+        return {
+            store,
+            projetos: computed(() => store.state.projetos)
         }
     }
 
