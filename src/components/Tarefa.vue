a8b4eeea329eceb662eab5874c38b6b66ddb0e97
RICHARD TANAKA <richard.tanaka@viavarejo.com.br>
2022-09-27 18:15:46 -0300

    [Done] 02. Organizando a store

--- src/components/Tarefa.vue
+++ src/components/Tarefa.vue
@@ -1,6 +1,6 @@
 <template>
     <Box>
-        <div class="columns">
+        <div class="columns clicavel" @click="tarefaClicada">
             <div class="column is-4">
                 {{tarefa.descricao || 'Tarefa sem descrição'}}
             </div>
@@ -23,6 +23,7 @@ import Box from './Box.vue';
 
 export default defineComponent({
     name: 'TarefaVue',
+    emits: ['aoTarefaClicada'],
     components: {
         Cronometro,
         Box
@@ -32,6 +33,17 @@ export default defineComponent({
             type: Object as PropType<ITarefa>,
             required: true
         }
-    }
+    },
+    methods: {
+        tarefaClicada() : void {
+            this.$emit('aoTarefaClicada', this.tarefa)
+        }
+    }  
 })
 </script>
+
+<style scoped>
+.clicavel {
+    cursor: pointer;
+}
+</style>
