f5fd550d19d4a6c1fd71c9df9a64e9e462d13d4b
RICHARD TANAKA <richard.tanaka@viavarejo.com.br>
2022-10-03 16:01:01 -0300

    [Done] 04. Reatividade

--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -1,12 +1,24 @@
 <template>
   <Formulario @aoSalvarTarefa="salvarTarefa" />
   <div class="lista">
-    <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" @aoTarefaClicada="selecionarTarefa" />
     <Box v-if="semTarefas">
       Você não está muito produtivo hoje
       <span class="has-text-weight-bold">:(</span>
     </Box>
-
+    <div class="field">
+      <p class="control has-icons-left">
+        <input class="input" type="text" placeholder="Digite para filtrar" v-model="filtro" />
+        <span class="icon is-small is-left">
+          <i class="fas fa-search"></i>
+        </span>       
+      </p>
+    </div>
+    <Tarefa 
+      v-for="(tarefa, index) in tarefas" 
+      :key="index" 
+      :tarefa="tarefa" 
+      @aoTarefaClicada="selecionarTarefa" 
+    />
     <div class="modal" :class="{'is-active' : tarefaSelecionada}" v-if="tarefaSelecionada">
       <div class="modal-background"></div>
       <div class="modal-card">
@@ -32,7 +44,7 @@
 </template>
   
 <script lang="ts">
-import { computed, defineComponent } from "vue";
+import { computed, defineComponent, ref, watchEffect } from "vue";
 import Formulario from "../components/Formulario.vue";
 import Box from "../components/Box.vue";
 import Tarefa from "../components/Tarefa.vue";
@@ -76,9 +88,22 @@ export default defineComponent({
     const store = useStore()
     store.dispatch(OBTER_TAREFAS)
     store.dispatch(OBTER_PROJETOS)
+
+    const filtro = ref('')
+
+    // const tarefas = computed(() => store.state.tarefa.tarefas.filter(
+    //     (t) => !filtro.value || t.descricao.includes(filtro.value)
+    //     )
+    //   );
+
+    watchEffect(() => {
+      store.dispatch(OBTER_TAREFAS, filtro.value)
+    })
+    
     return {
       tarefas: computed(() => store.state.tarefa.tarefas),
-      store
+      store,
+      filtro
     }
   }
 });
